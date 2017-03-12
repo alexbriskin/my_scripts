3d5487de7fd9530f36e4e18a4b4d657dc7875e6c
Alex Briskin <br.shurik@gmail.com>
2017-03-12 15:28:03 +0200

    setup_ubuntu: update

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -6,8 +6,16 @@ sudo apt-get update
 sudo apt install -y libc6:i386 lib32z1 libncurses5:i386 libstdc++6:i386
 
 sudo apt install -y dkms virtualbox-guest-dkms git vim  curl cscope thunar \
-	minicom mate-terminal xclip realpath cmake doxygen u-boot-tools atftp \
-	ssh g++ make mercurial pandoc openjdk-9-jre-headless smitools meld \
+	minicom mate-terminal xclip realpath cmake doxygen u-boot-tools atftp\
+	ssh g++ make mercurial pandoc openjdk-9-jre-headless smitools meld atftpd\
 	kdiff3 cscope patch libmpc3 ksh libsdl1.2-dev openssh-server telnetd \
 	xinetd  libgtk2.0-0:i386 libxtst6:i386
+
+
+sudo touch /var/log/atftpd.log
+sudo chmod 644 /var/log/atftpd.log
+[ ! -f /etc/default/atftpd ] && cat <<EOF | sudo tee /etc/default/atftpd
+USE_INETD=false
+OPTIONS="--tftpd-timeout 300 --retry-timeout 5 --mcast-port 1758 --mcast-addr 239.239.239.0-255 --mcast-ttl 1 --maxthread 100 --verbose=5 /tftpboot --logfile /var/log/atftpd.log"
+EOF
 
